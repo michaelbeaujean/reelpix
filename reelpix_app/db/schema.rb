d73bd1206e030b69d853c1fcfa742f0681743f95
mikebeaujean <mbeaujean88@gmail.com>
2014-03-28 11:55:21 -0400

    changed pw to pw digest on users table

--- reelpix_app/db/schema.rb
+++ reelpix_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328154239) do
+ActiveRecord::Schema.define(version: 20140328155254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,7 @@ ActiveRecord::Schema.define(version: 20140328154239) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.string   "password_confirmation"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
